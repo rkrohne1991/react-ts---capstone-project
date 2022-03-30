1b768d5a4ff6e76429da265f71346928f55dcb4d
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-30 13:07:19 +0200

    Adding useReducer to userContext

--- src/contexts/user.context.tsx
+++ src/contexts/user.context.tsx
@@ -5,7 +5,7 @@ import {
   ReactNode,
   SetStateAction,
   useEffect,
-  useState,
+  useReducer,
 } from "react";
 import {
   createUserDocumentFromAuth,
@@ -26,8 +26,46 @@ export const UserContext = createContext<AppContextInterface>(
   userContextDefaultValue
 );
 
+export enum UserActionType {
+  SET_CURRENT_USER = "set_current_user",
+}
+
+export interface SetCurrentUserAction {
+  type: UserActionType.SET_CURRENT_USER;
+  payload: User | null;
+}
+
+export type Action = SetCurrentUserAction;
+
+interface UsersState {
+  currentUser: User | null;
+}
+
+const initialState: UsersState = {
+  currentUser: null,
+};
+
+const userReducer = (state: UsersState = initialState, action: Action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case UserActionType.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }: { children: ReactNode }) => {
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, initialState);
+
+  const setCurrentUser = (user: any) => {
+    dispatch({ type: UserActionType.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
