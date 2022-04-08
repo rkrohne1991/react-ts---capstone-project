9b98217ccf79f46418657733869cb8849d489105
Rafal <rafal.krohne@hotmail.co.uk>
2022-04-08 11:18:30 +0200

    Removing reducer.utils due to not using it, updating firebase.utils keys

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -33,12 +33,12 @@ import { Categories } from "../../state/categories";
 import { ShopData } from "../../state/shop-data";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyAYAbo65mY08quMBE9MjtzSwOLYBMcCLRg",
-  authDomain: "react-crwn-clothing-db-31e3a.firebaseapp.com",
-  projectId: "react-crwn-clothing-db-31e3a",
-  storageBucket: "react-crwn-clothing-db-31e3a.appspot.com",
-  messagingSenderId: "633522564810",
-  appId: "1:633522564810:web:7680b581a9864728370e33",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
 
 const firebase: FirebaseApp = initializeApp(firebaseConfig);
--- src/utils/reducer/reducer.utils.ts
+++
@@ -1,1 +0,0 @@
-export const createAction = (type: any, payload: any) => ({ type, payload });
